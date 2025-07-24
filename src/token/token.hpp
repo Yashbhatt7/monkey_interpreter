9e3f97b78931663ba85f6720c8476a551f8308dc
Yashbhatt7 <yabh678@gmail.com>
2025-07-25 02:04:14 +0530

    token: initial implementation

---
+++ src/token/token.hpp
@@ -0,0 +1,99 @@
+#include<iostream>
+#include<unordered_map>
+
+enum class TokenType {
+    Illegal,
+    Eof,
+
+    // Identifiers + literals
+    Ident,
+    Int,
+
+    // Operators
+    Assign,
+    Plus,
+    Minus,
+    Bang,
+    Asterisk,
+    Slash,
+    Lt,
+    Gt,
+    Eq,
+    NotEq,
+
+    // Delimiters
+    Comma,
+    Semicolon,
+    LParen,
+    RParen,
+    LSquarly,
+    RSquarly,
+
+    // Keywords
+    Function,
+    Let,
+    True,
+    False,
+    If,
+    Else,
+    Return
+};
+
+struct Token {
+    TokenType type;
+    std::string literal;
+};
+
+class TokenTypeMap {
+public:
+    static std::string tokenTypeToString(TokenType type) {
+        static const std::unordered_map<TokenType, std::string> typeToString = {
+            {TokenType::Illegal, "ILLEGAL"},
+            {TokenType::Eof, "EOF"},
+            {TokenType::Ident, "IDENT"},
+            {TokenType::Int, "INT"},
+            {TokenType::Assign, "="},
+            {TokenType::Plus, "+"},
+            {TokenType::Minus, "-"},
+            {TokenType::Bang, "!"},
+            {TokenType::Asterisk, "*"},
+            {TokenType::Slash, "/"},
+            {TokenType::Lt, "<"},
+            {TokenType::Gt, ">"},
+            {TokenType::Eq, "=="},
+            {TokenType::NotEq, "!="},
+            {TokenType::Comma, ","},
+            {TokenType::Semicolon, ";"},
+            {TokenType::LParen, "("},
+            {TokenType::RParen, ")"},
+            {TokenType::LSquarly, "{"},
+            {TokenType::RSquarly, "}"},
+            {TokenType::Function, "FUNCTION"},
+            {TokenType::Let, "LET"},
+            {TokenType::True, "TRUE"},
+            {TokenType::False, "FALSE"},
+            {TokenType::If, "IF"},
+            {TokenType::Else, "ELSE"},
+            {TokenType::Return, "RETURN"},
+        };
+        return typeToString.at(type);
+    }
+
+    static TokenType lookupIdent(const std::string& ident) {
+        static const std::unordered_map<std::string, TokenType> keywords = {
+            { "fn", TokenType::Function },
+            { "let", TokenType::Let },
+            { "true", TokenType::True },
+            { "false", TokenType::False },
+            { "if", TokenType::If },
+            { "else", TokenType::Else },
+            { "return", TokenType::Return },
+        };
+
+        auto it = keywords.find(ident);
+        if(it != keywords.end()) {
+            return it->second;
+        }
+        return TokenType::Ident;
+    }
+};
