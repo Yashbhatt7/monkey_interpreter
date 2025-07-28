abfb094824d7d8be417bca34a375b2897ce42b77
Yashbhatt7 <yabh678@gmail.com>
2025-07-29 01:19:18 +0530

    update: testing further

--- tests/lexer_test.cpp
+++ tests/lexer_test.cpp
@@ -25,7 +25,21 @@ TEST(LexerTest, NextTokenFullTest) {
     //     10 != 9;
     // )";
 
-    std::string input  = "=+(){},;";
+
+    // std::string input  = "=+(){},;";
+
+
+    std::string input = R"(
+        let five = 5;
+        let ten = 10;
+
+        let add = fn(x, y) {
+            x + y;
+        };
+
+        let result = add(five, ten);
+    )";
+
 
     Lexer l(input);
 
@@ -35,15 +49,53 @@ TEST(LexerTest, NextTokenFullTest) {
     };
 
     std::vector<TestCase> tests = {
+        {TokenType::Let, "let"},
+        {TokenType::Ident, "five"},
         {TokenType::Assign, "="},
-        {TokenType::Plus, "+"},
+        {TokenType::Int, "5"},
+        {TokenType::Semicolon, ";"},
+        {TokenType::Let, "let"},
+        {TokenType::Ident, "ten"},
+        {TokenType::Assign, "="},
+        {TokenType::Int, "10"},
+        {TokenType::Semicolon, ";"},
+        {TokenType::Let, "let"},
+        {TokenType::Ident, "add"},
+        {TokenType::Assign, "="},
+        {TokenType::Function, "fn"},
         {TokenType::LParen, "("},
+        {TokenType::Ident, "x"},
+        {TokenType::Comma, ","},
+        {TokenType::Ident, "y"},
         {TokenType::RParen, ")"},
         {TokenType::LSquarly, "{"},
+        {TokenType::Ident, "x"},
+        {TokenType::Plus, "+"},
+        {TokenType::Ident, "y"},
+        {TokenType::Semicolon, ";"},
         {TokenType::RSquarly, "}"},
+        {TokenType::Semicolon, ";"},
+        {TokenType::Let, "let"},
+        {TokenType::Ident, "result"},
+        {TokenType::Assign, "="},
+        {TokenType::Ident, "add"},
+        {TokenType::LParen, "("},
+        {TokenType::Ident, "five"},
         {TokenType::Comma, ","},
+        {TokenType::Ident, "ten"},
+        {TokenType::RParen, ")"},
         {TokenType::Semicolon, ";"},
         {TokenType::Eof, ""},
+
+        // {TokenType::Assign, "="},
+        // {TokenType::Plus, "+"},
+        // {TokenType::LParen, "("},
+        // {TokenType::RParen, ")"},
+        // {TokenType::LSquarly, "{"},
+        // {TokenType::RSquarly, "}"},
+        // {TokenType::Comma, ","},
+        // {TokenType::Semicolon, ";"},
+        // {TokenType::Eof, ""},
     };
 
     for(size_t i = 0; i < tests.size(); ++i) {
