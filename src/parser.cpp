d2778688f9226aa01a4005ef880b28c81bb8afea
Yashbhatt7 <yabh678@gmail.com>
2025-08-04 23:43:14 +0530

    feat: initial implement Parser::nextToken() and ParseProgram()

--- src/parser.cpp
+++ src/parser.cpp
@@ -1,2 +1,17 @@
-#include<iostream>
+#include "parser.hpp"
+
+Parser::Parser(Lexer* lexer)
+    : l(lexer) {
+    nextToken();
+    nextToken();
+}
+
+void Parser::nextToken() {
+    curToken = peekToken;
+    peekToken = l->NextToken();
+}
+
+std::unique_ptr<Program> Parser::ParseProgram() {
+    return nullptr;
+}
 
