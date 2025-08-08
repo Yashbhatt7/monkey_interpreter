20c30346247e78a1b47476ab3cb3034c11276bd1
Yashbhatt7 <yabh678@gmail.com>
2025-08-09 01:51:55 +0530

    feat: initial implement parseLetStatement

--- src/parser.hpp
+++ src/parser.hpp
@@ -1,6 +1,7 @@
 #ifndef PARSER_HPP
 #define PARSER_HPP
 
+#include<sstream>
 #include "ast.hpp"
 #include "lexer.hpp"
 #include "token.hpp"
@@ -12,13 +13,19 @@ public:
     Token curToken;
     Token peekToken;
     Token token;
+    std::vector<std::string> errors;
 
-    explicit Parser(std::unique_ptr<Lexer> l);
+    explicit Parser(std::unique_ptr<Lexer> lexer);
     std::unique_ptr<Statement> parseStatement();
-    std::unique_ptr<Statement> parseLetStatement();
+    std::unique_ptr<LetStatement> parseLetStatement();
     void nextToken();
+    std::vector<std::string> Errors();
 
     std::unique_ptr<Program> ParseProgram();
+    bool expectPeek(TokenType t);
+    bool cutTokenIs(TokenType t);
+    bool peekTokenIs(TokenType t);
+    void peekErrors(TokenType t);
 };
 
 #endif // PARSER_HPP
