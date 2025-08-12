6608d1589ef22e2cf8f43a924b65e605b07e9816
Yashbhatt7 <yabh678@gmail.com>
2025-08-13 01:43:09 +0530

    feat: add String method implementation for Program, LetStatement, ReturnStatement, ExpressionStatement and for Identifier

--- src/ast.cpp
+++ src/ast.cpp
@@ -1,4 +1,5 @@
 #include<iostream>
+#include<sstream>
 #include "ast.hpp"
 
 std::string Program::TokenLiteral() const {
@@ -24,4 +25,56 @@ std::string ReturnStatement::TokenLiteral() const {
 std::string ExpressionStatement::TokenLiteral() const {
     return token.literal;
 }
+
+std::string Program::String() const {
+    std::ostringstream out;
+    for (const auto& s : Statements) {
+        out << s->String();
+    }
+
+    return out.str();
+}
+
+std::string LetStatement::String() const {
+    std::ostringstream out;
+
+    out << TokenLiteral() << " ";
+    out << Name->String();
+    out << " = ";
+
+    if (Value != nullptr) {
+        out << Value->String();
+    }
+
+    out << ";";
+
+    return out.str();
+}
+
+std::string ReturnStatement::String() const {
+    std::ostringstream out;
+
+    out << TokenLiteral() << " ";
+
+    if (ReturnValue != nullptr) {
+        out << ReturnValue->String();
+    }
+
+    out << ";";
+
+    return out.str();
+}
+
+std::string ExpressionStatement::String() const {
+    if (Expression != nullptr) {
+        return Expression->String();
+    }
+
+    return "";
+}
+
+std::string Identifier::String() const {
+    return Value;
+}
+
 
