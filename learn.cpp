8ad849127e507e3bef985707a9594db273eb319e
Yashbhatt7 <yabh678@gmail.com>
2025-08-14 23:54:44 +0530

    learn: intuition test + learn

---
+++ learn.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+#include<memory>
+
+class Lexer {
+public:
+    std::unique_ptr<Lexer> a;
+    char ch;
+public:
+    // void call(Lexer* obj) {
+    //     obj->ch = 'a';
+    // }
+};
+
+int main() {
+    // Lexer lexer;
+    // lexer.call(&lexer);
+    int chch = 13;
+
+    std::unique_ptr<Lexer> b;
+    // std::unique_ptr<Lexer> a = std::make_unique<Lexer>(ch, std::move(b));
+    std::unique_ptr<Lexer> d = std::make_unique<Lexer>(std::move(b), chch);
+
+    std::cout << "ch: " << d->ch << std::endl;
+
+    std::cin.get();
+}
