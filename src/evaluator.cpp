21643bd966f877046ea4c41bfa523721fcf71c3e
Yashbhatt7 <yabh678@gmail.com>
2025-09-21 23:53:29 +0530

    feat: add Eval function for evaluation & evalStatement: IntegerLiteral

---
+++ src/evaluator.cpp
@@ -0,0 +1,62 @@
+#include "evaluator.hpp"
+
+std::unique_ptr<Object> evalStatements(const std::vector<std::unique_ptr<Statement>>& stmts) {
+    std::unique_ptr<Object> result;
+
+    for (const auto& statement : stmts) {
+        result = Eval(statement.get());
+    }
+
+    return result;
+}
+
+// Experiment (till now enum design is working)
+std::unique_ptr<Object> Eval(Node* node) {
+    switch (node->Type()) {
+        // Program
+        case NodeType::PROGRAM: {
+            auto program = static_cast<Program*>(node);
+            return evalStatements(program->Statements);
+        }
+
+        // Statements
+        case NodeType::EXPRESSION_STATEMENT: {
+            auto exprStmt = static_cast<ExpressionStatement*>(node);
+            return Eval(exprStmt->Expression.get());
+        }
+
+        // Expressions
+        case NodeType::INTEGER_LITERAL: {
+            auto intLiteral = static_cast<IntegerLiteral*>(node);
+            return std::make_unique<Integer>(intLiteral->Value);
+        }
+        case NodeType::BOOLEAN_LITERAL: {
+            auto boolLiteral = static_cast<BooleanLiteral*>(node);
+            return std::make_unique<Boolean>(boolLiteral->Value);
+        }
+    }
+
+    return nullptr;
+}
+
+// std::unique_ptr<Object> Eval(Node* node) {
+//     // Program
+//     if (auto program = dynamic_cast<Program*>(node)) {
+//         return evalStatements(program->Statements);
+//     }
+//
+//     // Statements
+//     if (auto exprStmt = dynamic_cast<ExpressionStatement*>(node)) {
+//         return Eval(exprStmt->Expression.get());
+//     }
+//
+//     // Expressions
+//     if (auto intLiteral = dynamic_cast<IntegerLiteral*>(node)) {
+//         return std::make_unique<Integer>(intLiteral->Value);
+//     } else if (auto boolLiteral = dynamic_cast<BooleanLiteral*>(node)) {
+//         return std::make_unique<Boolean>(boolLiteral->Value);
+//     }
+//
+//     return nullptr;
+// }
+
