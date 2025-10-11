2df7229578ba8fc91fc534107f2e24c5849b1cb9
Yashbhatt7 <yabh678@gmail.com>
2025-10-11 12:28:53 +0530

    fix and changes add NewEnclosedEnvironment

--- src/environment.hpp
+++ src/environment.hpp
@@ -10,14 +10,17 @@ class Environment {
 private:
     std::unordered_map<std::string, std::unique_ptr<Object>> store;
 public:
-    Environment() = default;
+    std::shared_ptr<Environment> outer;
+
+    Environment() : outer(nullptr) {}
 
     Object* Get(const std::string& name);
 
     Object* Set(const std::string& name, std::unique_ptr<Object> val);
 };
 
-std::unique_ptr<Environment> NewEnvironment();
+std::shared_ptr<Environment> NewEnvironment();
+std::shared_ptr<Environment> NewEnclosedEnvironment(std::shared_ptr<Environment> outer);
 
 #endif
 
